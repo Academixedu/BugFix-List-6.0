b578892416709ea084b68c8b89b8f8d066a2f863
chdkumar <chokkakulakumar1123@gmail.com>
2024-07-24 19:22:20 +0530

    BugFixing

--- Bug3.java
+++ Bug3.java
@@ -5,6 +5,8 @@ public class Bug3 {
         ArrayList<Integer> list = new ArrayList<>();
         list.add(1);
         list.add(2);
-        list.set(2, 3); 
+        list.add(4);
+        list.set(2, 3);
+        System.out.println(list); 
     }
 }
