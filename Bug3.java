fea85f983839528c24298c4a8e46987e019e7227
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-07-24 13:05:16 +0530

    Create Bug3.java

---
+++ Bug3.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Bug3 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.set(2, 3); 
+    }
+}
