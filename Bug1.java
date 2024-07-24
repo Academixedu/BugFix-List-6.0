879505ccb395b4e5adc41edb15a06982a079231c
manicharanchindam <manicharanchindam@email.com>
2024-07-24 20:51:11 +0530

    bug fixed

--- Bug1.java
+++ Bug1.java
@@ -6,6 +6,6 @@ public class Bug1 {
         list.add(1);
         list.add(2);
         list.add(3);
-        System.out.println(list.get(3));
+        System.out.println(list.get(2));//Here 3 represents the index of the List , Their are upto 0-2 index only ,so its giving error
     }
 }
--- Bug3.java
+++ Bug3.java
@@ -5,6 +5,7 @@ public class Bug3 {
         ArrayList<Integer> list = new ArrayList<>();
         list.add(1);
         list.add(2);
-        list.set(2, 3); 
+        list.set(1, 3); //Here index starts from 0-1 
+        System.out.println(list);
     }
 }
