890fbf2dd97c580b64b80b543a5b066f89316820
Naveentogaru1 <togarunaveen1@gmail.com>
2024-07-26 18:49:57 +0530

    BugFix-List-6.0/Bug1.java

--- Bug1.java
+++ Bug1.java
@@ -6,6 +6,6 @@ public class Bug1 {
         list.add(1);
         list.add(2);
         list.add(3);
-        System.out.println(list.get(3));
+        System.out.println(list.get(1));
     }
 }
--- Bug2.java
+++ Bug2.java
@@ -10,8 +10,8 @@ public class Bug2 {
 
         Iterator<Integer> iterator = list.iterator();
         while (iterator.hasNext()) {
-            list.remove(1);
-            System.out.println(iterator.next());
+            list.remove(0);
+            System.out.println(list);
         }
     }
 }
