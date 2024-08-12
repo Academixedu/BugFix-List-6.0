098a18c831032d7d54bbe71a9155827a3bf8ecb5
Shashi preetham Nathi <nathishashipreetham@gmail.com>
2024-08-12 17:40:41 +0530

    Update Bug1.java

--- Bug1.java
+++ Bug1.java
@@ -1,11 +1,13 @@
 import java.util.ArrayList;
 
-public class Bug1 {
+public class prac {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
         list.add(1);
         list.add(2);
         list.add(3);
-        System.out.println(list.get(3));
+        for(Integer e:list){
+        System.out.println(list);
+        }
     }
 }
