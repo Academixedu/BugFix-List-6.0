cb599d4d8cd5ca60de57d368ae7cc7754ec3be69
goutham120203 <gouthampaladugu12@gmail.com>
2024-08-12 18:53:55 +0530

    goutham

--- Bug1.java
+++ Bug1.java
@@ -6,6 +6,7 @@ public class Bug1 {
         list.add(1);
         list.add(2);
         list.add(3);
-        System.out.println(list.get(3));
+
+        System.out.println(list.get(2));
     }
 }
--- Bug3.java
+++ Bug3.java
@@ -5,6 +5,10 @@ public class Bug3 {
         ArrayList<Integer> list = new ArrayList<>();
         list.add(1);
         list.add(2);
+        list.add(2);
         list.set(2, 3); 
+        for (Integer integer : list) {
+            System.out.println(integer);
+           }
     }
 }
