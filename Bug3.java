2078fd6bd582ea42a19284b1ab1d1bc1c3a2a8bd
2110Abhishek <agchoudhari2110@gmail.com>
2024-08-12 19:57:41 +0530

    Abhishek

--- Bug3.java
+++ Bug3.java
@@ -1,10 +1,16 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Bug3 {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
         list.add(1);
         list.add(2);
-        list.set(2, 3); 
+        list.set(1,3);
+
+        Iterator<Integer>i=list.iterator();
+        while (i.hasNext()) {
+            System.out.println(i.next());
+        }
     }
 }
