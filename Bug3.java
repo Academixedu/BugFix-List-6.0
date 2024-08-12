d21d86b5290c2020f6b737dc222a47559fca04fc
Shashi preetham Nathi <nathishashipreetham@gmail.com>
2024-08-12 17:53:12 +0530

    Update Bug3.java

--- Bug3.java
+++ Bug3.java
@@ -1,10 +1,14 @@
 import java.util.ArrayList;
 
-public class Bug3 {
+public class prac {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
         list.add(1);
         list.add(2);
+        list.add(0);
         list.set(2, 3); 
+        for(Integer e:list){
+          System.out.println(e);
+        }
     }
 }
