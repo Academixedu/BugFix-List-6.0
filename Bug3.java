a6d826491c87d87e10b0e6d26316e2fc0cbd2744
Bharathmanne18 <bharathmanne19@gmail.com>
2024-08-12 18:52:47 +0530

    Update Bug3.java

--- Bug3.java
+++ Bug3.java
@@ -1,10 +1,13 @@
+ package collection;
 import java.util.ArrayList;
 
 public class Bug3 {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
+        list.add(0);
         list.add(1);
         list.add(2);
         list.set(2, 3); 
+        System.out.println(list);
     }
 }
