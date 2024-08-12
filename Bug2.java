9ddad460933567a9500b97e3d355cc7a51aa5545
ratan-17 <rkmule2002@gmail.com>
2024-08-12 19:09:02 +0530

    Ratan

--- Bug2.java
+++ Bug2.java
@@ -1,16 +1,19 @@
 import java.util.ArrayList;
 import java.util.Iterator;
 
-public class Bug2 {
-    public static void main(String[] args) {
+public class Bug2 
+{
+    public static void main(String[] args) 
+    {
         ArrayList<Integer> list = new ArrayList<>();
         list.add(1);
         list.add(2);
         list.add(3);
+        list.remove(1);
 
         Iterator<Integer> iterator = list.iterator();
-        while (iterator.hasNext()) {
-            list.remove(1);
+        while (iterator.hasNext())
+         {
             System.out.println(iterator.next());
         }
     }
