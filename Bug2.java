30b70c100b65df5a2b9da1f77ec68fcfab6fe747
revathivinnakota90@gmail.com <revathivinnakota90@gmail.com>
2024-09-03 18:21:54 +0530

    Revathi

--- Bug2.java
+++ Bug2.java
@@ -1,17 +1,18 @@
 import java.util.ArrayList;
 import java.util.Iterator;
-
 public class Bug2 {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
         list.add(1);
         list.add(2);
         list.add(3);
-
         Iterator<Integer> iterator = list.iterator();
         while (iterator.hasNext()) {
-            list.remove(1);
+            
             System.out.println(iterator.next());
         }
+        System.out.println("Modified list");
+        list.remove(1);
+        System.out.println(list);
     }
 }
