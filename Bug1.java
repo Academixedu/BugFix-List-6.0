be399fe70796d109afa8eee90e4289a6cbc430a3
sheik-noorahammed <your@nsyed6348@gmail.com>
2024-09-03 19:44:52 +0530

    save

--- Bug1.java
+++ Bug1.java
@@ -6,6 +6,10 @@ public class Bug1 {
         list.add(1);
         list.add(2);
         list.add(3);
-        System.out.println(list.get(3));
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
     }
 }
+
